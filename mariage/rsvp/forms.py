570e7896d8426efef170bf5c80365f530dc52ede
Gregory Favre <gregory.favre@gmail.com>
2012-12-14 23:21:50 +0100

    rsvp form is now correctly aligned

--- mariage/rsvp/forms.py
+++ mariage/rsvp/forms.py
@@ -6,9 +6,9 @@ from cmsplugin_contact.forms import ContactForm
 
 choices = ((1, 'Oui'), (0, 'Non'),)
 class RSVPForm(forms.Form):
-    name = forms.CharField(widget=forms.Textarea(), required=True)
+    name = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'class': 'span6'}), required=True)
     presence = forms.ChoiceField(choices=choices, widget=forms.RadioSelect, initial=1, required=True)
-    content = forms.CharField(widget=forms.Textarea(), required = False)
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 6, 'class': 'span6'}), required = False)
     email = forms.CharField(max_length=100, required=False)
     subject = forms.CharField(max_length=100, required=False)
 
