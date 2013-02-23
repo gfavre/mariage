ba9f8ee12e651d72f88bc965af04a5f4894b30f6
Gregory Favre <gregory.favre@gmail.com>
2013-02-23 21:09:55 +0100

    added sitemaps

--- mariage/urls.py
+++ mariage/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.http import HttpResponse
 
-
+from cms.sitemaps import CMSSitemap
 from photologue.models import Photo
 
 admin.autodiscover()
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     (r'^tinymce/', include('tinymce.urls')),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^google7f92da28d3dd66e7\.html$', lambda r: HttpResponse("google-site-verification: google7f92da28d3dd66e7.html", mimetype="text/plain")),
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^', include('cms.urls')),
 )
 
