37ceafb930269715b0a84e113103e4b8e2ee7e3a
Gregory Favre <gregory.favre@gmail.com>
2013-03-09 21:20:28 +0100

    changed room type

--- mariage/hotel/forms.py
+++ mariage/hotel/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from cmsplugin_contact.forms import ContactForm
 
 
-choices = ((1, 'Simple'), (2, 'Double'), (3, 'Double (lits jumeaux)'),)
+choices = ((1, 'Double'), (2, 'Double (lits jumeaux)'), (3, 'Pavillon'),)
 class HotelForm(forms.Form):
     name = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'class': 'span6'}), required=True)
     chambre = forms.ChoiceField(choices=choices, widget=forms.RadioSelect, initial=1, required=True)
